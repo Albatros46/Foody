4853e82eaa1f2c0f3fe18099258b0117727bc4b4
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2024-07-03 19:39:27 +0200

    2-Entity Layer

---
+++ Foody.Entities/Concrete/Feature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.Entities.Concrete
+{
+    public class Feature
+    {
+        public int featureId { get; set; }
+        public string TItle { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; } = true;
+    }
+}
---
+++ Foody.Entities/Concrete/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.Entities.Concrete
+{
+    public class Review
+    {
+        public int reviewId { get; set; }
+        public string CustomerName { get; set; }
+        public string Titel { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
