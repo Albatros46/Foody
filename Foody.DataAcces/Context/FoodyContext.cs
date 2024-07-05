4676cab7e6a5935d368604d0c61d0e3b153979ef
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2024-07-05 11:52:11 +0200

    3-Category und Product DTOs beendet dann habe ich die Table Migration gemacht.

---
+++ Foody.DataAcces/Context/FoodyContext.cs
@@ -0,0 +1,30 @@
+﻿using Foody.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foody.DataAcces.Context
+{
+    public class FoodyContext : DbContext
+    {
+        //public FoodyContext(DbContextOptions options) : base(options)
+        //{
+
+        //}
+        protected override void OnConfiguring(DbContextOptionsBuilder opt)
+        {
+            opt.UseSqlServer("Server=ALBATROS46;initial catalog=FoodyDb;integrated security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+    }
+}
